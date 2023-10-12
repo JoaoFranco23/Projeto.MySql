2cb5bd9eebabe39aed9f0ee43c6b7730ee144b07
JoaoFranco23 <147751659+JoaoFranco23@users.noreply.github.com>
2023-10-12 16:18:17 -0300

    Update Locação.sql

--- Locação.sql
+++ Locação.sql
@@ -1,5 +1,6 @@
-create database Aluguel_Carro;
+create database Aluguel_Carro; --tabela de locação de veiculos
 
+--Tabela de carros voltado pra locação dos veiculos 
 create table Carros(
 	Id int identity(1, 1),
 	Modelo varchar(100) not null, 
@@ -7,11 +8,12 @@ create table Carros(
 	Marca varchar(100) not null,
 	constraint Carros_Id_pk primary key(Id),
 	constraint Carros_Placa unique(Placa),
-	constraint Carros_Modelo check(Modelo in('Suv', 'Sedan', 'Hibrido', 'Hatch'))
+	constraint Carros_Modelo check(Modelo in('Suv', 'Sedan', 'Hibrido', 'Hatch'));
 ); 
 
 select * from Carros;  
 
+--Tabela voltado pra cadastro do cliente 
 create table Cliente(
 	Id_Cliente int identity(1, 1),
 	Nome Varchar(100) not null,
@@ -22,6 +24,7 @@ create table Cliente(
 	constraint Cliente_CNH_un unique(CNH)
 );
 
+--Aluguel do veiculo, conta com a data de locaçao de inicio até finalização desta locação, foi utilizado uma constraint 
 create table Locacao(
 	Id_Locacao int identity(1, 1), 
 	Data_Locacao datetime not null, 
@@ -29,8 +32,19 @@ create table Locacao(
 	Modelo varchar(100) not null,
 	Forma_Pagamento varchar(100) not null,
 	Quantidade_Veiculos_Alugados int not null,
-	Preco numeric constraint Preco_Veiculo check(Preco > 0)
+	Telefone int,
+	Preco numeric constraint Preco_Veiculo check(Preco > 0) --O preço gera uma restrição a determinado valor inserido
 	constraint Locacao_Id_Locacao_pk primary key(Id_Locacao),
 	constraint Locacao_Quantidade_Veiculos_Alugados_fk foreign key(Quantidade_Veiculos_Alugados) references Locacao(Id_Locacao),
-
+	constraint Locacao_Telefone_fk foreign key(Telefone) references Locacao (Id_Locacao)
 );
+
+select * from Carros; --Seleciona a tabela do banco de dados criado
+
+use Aluguel_Carro; --banco de dados criado 
+
+set IDENTITY_INSERT Aluguel_Carro on;
+
+select * from Cliente; 
+
+insert into Cliente ([Id_Cliente], [Nome], [Sexo], [CNH], [Forma_Pagamento]) values (1, 'Cleide', 'F', '3105808097', 'Dinheiro');
